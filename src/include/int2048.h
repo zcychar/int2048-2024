eb0e1ef40a6fa2f52e92a9ecd211d510ac335d5b
zcychar <zhang.77274@gmail.com>
2024-11-11 07:41:03 +0800

    change

--- src/include/int2048.h
+++ src/include/int2048.h
@@ -19,12 +19,13 @@
 // 请不要使用 using namespace std;
 
 namespace sjtu {
+  constexpr long long p = 1e8;
+  constexpr long long base = 8;
+
   class int2048 {
     std::vector<long long> digit;
     bool symbol = false; //true=negative
     int length = 0;
-
-    static constexpr long long p = 1e8;
 
   public:
     // 构造函数
@@ -63,7 +64,6 @@ namespace sjtu {
 
     friend bool isAbsLess(const int2048 &, const int2048 &);
 
-    friend bool isAbsLeq(const int2048 &, const int2048 &);
     // ===================================
     // Integer2
     // ===================================
@@ -76,11 +76,11 @@ namespace sjtu {
 
     int2048 &operator+=(const int2048 &);
 
-    friend int2048 operator+(int2048, const int2048 &);
+    friend int2048 operator+(const int2048 &, const int2048 &);
 
     int2048 &operator-=(const int2048 &);
 
-    friend int2048 operator-(const int2048&, const int2048 &);
+    friend int2048 operator-(const int2048 &, const int2048 &);
 
     int2048 &operator*=(const int2048 &);
 
@@ -109,14 +109,20 @@ namespace sjtu {
     friend bool operator<=(const int2048 &, const int2048 &);
 
     friend bool operator>=(const int2048 &, const int2048 &);
+
+    int2048 &operator<<=(long long);
+
+    int2048 &operator>>=(long long);
   };
 
   inline int2048::int2048() {
     length = 1;
+    digit.clear();
     digit.push_back(0);
   }
 
   inline int2048::int2048(long long num) {
+    digit.clear();
     if (num < 0) {
       symbol = true;
       num = -num;
@@ -132,6 +138,35 @@ namespace sjtu {
     }
   }
 
+  inline int2048 &int2048::operator<<=(long long num) {
+    digit.resize(++length);
+    digit[0]<<=1;
+    for (int i = 1;i<length;++i) {
+      digit[i]<<=1;
+      if(digit[i-1]>=p) {
+        digit[i-1]-=p;
+        digit[i]++;
+      }
+    }
+    if (!digit[length - 1] && length > 1) {
+      length--;
+    }
+    return *this;
+  }
+
+  inline int2048 &int2048::operator>>=(long long num) {
+    for (int i = length - 1; i >= 0; --i) {
+      if ((digit[i] & 1) && i >= 1) {
+        digit[i - 1] += p;
+      }
+      digit[i] >>= 1;
+    }
+    if (!digit[length - 1] && length > 1) {
+      length--;
+    }
+    return *this;
+  }
+
   inline int2048::int2048(const std::string &str) {
     read(str);
   }
@@ -143,25 +178,24 @@ namespace sjtu {
     symbol = false;
     if (str[0] == '-') {
       symbol = true;
-      length = (str.length() + 6) / 8;
-      if (str[1] == '0') {
-        digit.push_back(0);
-        symbol = false;
-        length = 1;
-        return;
-      }
-    } else {
-      length = (str.length() + 7) / 8;
     }
+    int starting=0,ending=str.length()-1;
+    while((str[starting]=='-'||str[starting]=='0')&&starting<=ending)starting++;
+    if(starting>ending) {
+      symbol=false;
+      length=1;
+      digit.push_back(0);
+      return;
+    }
+    length=(ending-starting)/base+1;
     digit.resize(length);
-    int pointer = str.length() - 1, cnt = 0;
     for (int i = 0; i < length - 1; ++i) {
-      for (int j = 7; j >= 0; --j) {
-        digit[i] = digit[i] * 10 + (str[pointer - j] - '0');
+      for (int j = base - 1; j >= 0; --j) {
+        digit[i] = digit[i] * 10 + (str[ending - j] - '0');
       }
-      pointer -= 8;
+      ending -= base;
     }
-    for (int i = (symbol ? 1 : 0); i <= pointer; ++i) {
+    for (int i = starting; i <= ending; ++i) {
       digit[length - 1] = digit[length - 1] * 10 + (str[i] - '0');
     }
   }
@@ -170,16 +204,16 @@ namespace sjtu {
     if (symbol) {
       std::cout << '-';
     }
-    printf("%lld", digit[length - 1]);
+    std::cout<<digit[length-1];
     for (int i = length - 2; i >= 0; --i) {
-      printf("%.8lld", digit[i]);
+      std::cout<<std::setw(base)<<std::setfill('0')<<digit[i];
     }
   }
 
 
   inline int2048 &int2048::add(const int2048 &other) {
     if (symbol == other.symbol) {
-      int res = 0;
+      long long res = 0;
       length = std::max(length, other.length);
       digit.resize(length);
       for (int i = 0; i < other.length; ++i) {
@@ -191,6 +225,7 @@ namespace sjtu {
         digit[i] = digit[i] + res;
         res = digit[i] / p;
         digit[i] %= p;
+        if (!res)break;
       }
       if (res) {
         digit.push_back(res);
@@ -230,6 +265,7 @@ namespace sjtu {
           }
         }
         for (int i = other.length; i < length; ++i) {
+          if (!res)break;
           digit[i] = digit[i] - res;
           res = 0;
           if (digit[i] < 0) {
@@ -250,9 +286,12 @@ namespace sjtu {
     return *this;
   }
 
+
+
   inline int2048 add(int2048 first, const int2048 &second) {
     return first.add(second);
   }
+
 
   inline int2048 &int2048::minus(const int2048 &other) {
     if (symbol != other.symbol) {
@@ -267,6 +306,7 @@ namespace sjtu {
         digit[i] %= p;
       }
       for (int i = other.length; i < length; ++i) {
+        if (!res)break;
         digit[i] = digit[i] + res;
         res = digit[i] / p;
         digit[i] %= p;
@@ -308,6 +348,7 @@ namespace sjtu {
           }
         }
         for (int i = other.length; i < length; ++i) {
+          if (!res)break;
           digit[i] = digit[i] - res;
           res = 0;
           if (digit[i] < 0) {
@@ -350,60 +391,43 @@ namespace sjtu {
     return false;
   }
 
-  inline bool isAbsLeq(const int2048 &first, const int2048 &second) {
-    if (first.length < second.length) {
-      return true;
-    }
-    if (first.length > second.length) {
-      return false;
-    }
-    for (int i = first.length - 1; i >= 0; --i) {
-      if (first.digit[i] < second.digit[i]) {
-        return true;
-      }
-      if (first.digit[i] > second.digit[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
 
   inline int2048 int2048::operator+() const {
     return *this;
   }
 
   inline int2048 int2048::operator-() const {
-    int2048 result=*this;
-    result.symbol^=1;
+    int2048 result = *this;
+    if(result==0)return result;
+    result.symbol ^= 1;
     return result;
   }
 
   inline std::istream &operator>>(std::istream &in, int2048 &number) {
     std::string s;
-    in>>s;
+    in >> s;
     number.read(s);
     return in;
   }
 
   inline std::ostream &operator<<(std::ostream &out, const int2048 &number) {
     if (number.symbol) {
-      out<< '-';
+      out << '-';
     }
-    printf("%lld",number. digit[number.length - 1]);
+    out<<number.digit[number.length - 1];
     for (int i = number.length - 2; i >= 0; --i) {
-      out<<std::setw(8)<<std::setfill('0')<<number.digit[i];
+      out << std::setw(base) << std::setfill('0') << number.digit[i];
     }
     return out;
   }
 
+  inline int2048 &int2048::operator=(const int2048 &rhs) = default;
 
-  inline int2048 &int2048::operator=(const int2048 &str) = default;
-
-  inline int2048 operator+(int2048 lhs, const int2048 &rhs) {
-    return add(lhs,rhs);
+  inline int2048 operator+(const int2048 &lhs, const int2048 &rhs) {
+    return add(lhs, rhs);
   }
 
-  inline int2048 operator-(const int2048& lhs,const int2048 &rhs) {
+  inline int2048 operator-(const int2048 &lhs, const int2048 &rhs) {
     return minus(lhs, rhs);
   }
 
@@ -416,48 +440,163 @@ namespace sjtu {
   }
 
   inline bool operator==(const int2048 &lhs, const int2048 &rhs) {
-    if(lhs.symbol!=rhs.symbol) {
+    if (lhs.symbol != rhs.symbol) {
       return false;
     }
-    if(lhs.length!=rhs.length) {
+    if (lhs.length != rhs.length) {
       return false;
     }
-    if(lhs.digit!=rhs.digit) {
-      return false;
+    for (int i = lhs.length - 1; i >= 0; --i) {
+      if(lhs.digit[i]!=rhs.digit[i]) {
+        return false;
+      }
     }
     return true;
   }
 
   inline bool operator!=(const int2048 &lhs, const int2048 &rhs) {
-    return operator==(lhs,rhs)^1;
+    return 1^operator==(lhs, rhs) ;
   }
 
   inline bool operator<(const int2048 &lhs, const int2048 &rhs) {
-    if(lhs.symbol!=rhs.symbol) {//symbol=true: negative
+    if (lhs.symbol != rhs.symbol) {
+      //symbol=true: negative
       return lhs.symbol;
     }
-    if(lhs.symbol) {
-      return isAbsLeq(lhs,rhs)^1;
+    if (lhs.symbol) {
+      return isAbsLess(rhs,lhs);
     }
-    return isAbsLess(lhs,rhs);
+    return isAbsLess(lhs, rhs);
   }
 
   inline bool operator>(const int2048 &lhs, const int2048 &rhs) {
-    return operator<(rhs,lhs);
+    return operator<(rhs, lhs);
   }
 
   inline bool operator<=(const int2048 &lhs, const int2048 &rhs) {
-    return operator>(lhs,rhs)^1;
+    return operator>(lhs, rhs) ^ 1;
   }
 
   inline bool operator>=(const int2048 &lhs, const int2048 &rhs) {
-    return operator<(lhs,rhs)^1;
+    return operator<(lhs, rhs) ^ 1;
   }
 
 
+  inline int2048 &int2048::operator*=(const int2048 &rhs) {
+    if(*this==0||rhs==0)return *this;
+    // if(rhs.length==1) {
+    //   long long num=rhs.digit[0];
+    //   digit.resize(length+1);
+    //   for(int i=0;i<length;++i) {
+    //     digit[i]*=num;
+    //     digit[i+1]+=digit[i]/p;
+    //     digit[i]%=p;
+    //   }
+    //   if(digit[length]) {
+    //     length++;
+    //   }
+    //   symbol=(symbol ^ (rhs.symbol));
+    //   return *this;
+    // }
+    int2048 result;
+    result.symbol = (symbol ^ (rhs.symbol));
+    result.length = length + rhs.length - 1;
+    result.digit.resize(length + rhs.length + 1);
+    for (int i = 0; i < result.length; ++i) {
+      int starting = std::max(i - rhs.length + 1, 0);
+      int ending = std::min(length - 1, i);
+      for (int j = starting; j <= ending; ++j) {
+        result.digit[i] += digit[j] * rhs.digit[i - j];
+      }
+      if (result.digit[i] >= p) {
+        result.digit[i + 1] += result.digit[i] / p;
+        result.digit[i] %= p;
+      }
+    }
+    if (result.digit[result.length]) {
+      result.length++;
+    }
+    *this = result;
+    return *this;
+  }
+
+  inline int2048 operator*(int2048 lhs, const int2048 &rhs) {
+    return lhs.operator*=(rhs);
+  }
 
 
+  inline int2048 &int2048::operator/=(const int2048 &rhs) {
+    if(digit[0]==0&&length==1) {
+      symbol=false;
+      return *this;
+    }
 
+    if(isAbsLess(*this,rhs)) {
+      if(symbol==rhs.symbol) return *this=0;
+      return *this=(-1);
+    }
+
+    if(rhs.length==1) {
+      long long div=rhs.digit[0];
+      int2048 tmp=*this;
+      tmp.symbol=false;
+      long long res=0;
+      res=tmp.digit[tmp.length-1]%div;
+      tmp.digit[tmp.length-1]/=div;
+      for(int i=tmp.length-2;i>=0;--i) {
+        tmp.digit[i]+=res*p;
+        res=tmp.digit[i]%div;
+        tmp.digit[i]/=div;
+      }
+      if (!tmp.digit[length - 1] && tmp.length > 1) {
+        tmp.length--;
+      }
+      tmp.symbol = (symbol ^ (rhs.symbol));
+      if(tmp.symbol&&res>0) {
+        tmp-=1;
+      }
+      *this=tmp;
+      return *this;
+    }
+
+    int2048 result(0), tmp=*this, div = rhs, num(1);
+    div.symbol = false;
+    tmp.symbol = false;
+
+    while(div<tmp) {
+      div<<=1;
+      num<<=1;
+    }
+    while(num>=1) {
+      if(tmp>=div) {
+        tmp-=div;
+        result+=num;
+      }
+      div>>=1;
+      num>>=1;
+    }
+
+    result.symbol = (symbol ^ (rhs.symbol));
+    if(result.symbol&&tmp>0) {
+      result-=1;
+    }
+    *this = result;
+    return *this;
+  }
+
+  inline int2048 operator/(int2048 lhs, const int2048 &rhs) {
+    return lhs.operator/=(rhs);
+  }
+
+  inline int2048 &int2048::operator%=(const int2048 &rhs) {
+
+    *this=*this-(*this/rhs)*rhs;
+    return *this;
+  }
+
+  inline int2048 operator%(int2048 lhs, const int2048 &rhs) {
+    return lhs.operator%=(rhs);
+  }
 } // namespace sjtu
 
 #endif
